c80ec6e9a324264421085c16c0bbb8982b65095b
fanyijie <fanyijie@frogsing.com>
2019-10-15 12:09:07 +0800

    ok

--- config/index.js
+++ config/index.js
@@ -68,13 +68,13 @@ module.exports = {
      */
     productionSourceMap: true,
     // https://webpack.js.org/configuration/devtool/#production
-    devtool: 'source-map',
-
+ //   devtool: 'source-map',
+    devtool:'cheap-module-eval-source-map',
+    productionGzip: false,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
     // npm install --save-dev compression-webpack-plugin
-    productionGzip: false,
     productionGzipExtensions: ['js', 'css'],
 
     // Run the build command with an extra argument to
