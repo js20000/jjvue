a5f38e8993c402c8244d09f4d0437b268ebdd5d8
fanyijie <fanyijie@frogsing.com>
2020-07-01 18:41:04 +0800

    1.154

--- src/components/jj/button/link.vue
+++ src/components/jj/button/link.vue
@@ -1,6 +1,6 @@
 <!--suppress ALL -->
 <template>
-  <el-link v-re-click  @click="on_click"  type="primary"  v-if="showFlag && hasP "  :disabled="disabled" style="padding-right: 5px;">{{ btn.label }}</el-link>
+  <el-link v-re-click  @click="on_click"  type="primary"  v-if="showFlag && hasP "  :disabled="disabled" style="padding-right: 5px;">{{ label }}</el-link>
 <!--  <el-button type="text" :disabled="disabled"  plain size="small" @click="$emit('click')"  v-if="showFlag && hasP " style="padding-right: 5px;" >-->
 <!--    {{ btn.label }}-->
 <!--  </el-button>-->
@@ -47,12 +47,16 @@
       } else { this.$set(this.btn, 'disabled', false) }
     },
     methods: {
-      on_click(){
+      on_click() {
         this.$emit('click')
       }
     },
     computed: {
-
+      label() {
+        if (typeof this.btn.label === 'function') {
+          return !this.btn.label.apply(this, [{ row: this.row, btn: this.btn }])
+        } else { return this.btn.label }
+      },
       showFlag() {
         if (typeof this.btn.hidden === 'function') {
           return !this.btn.hidden.apply(this, [{ row: this.row, btn: this.btn }])
