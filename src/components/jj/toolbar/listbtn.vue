2e2b939b1f16bc6b19436ac8985afafae9676e4a
范益杰 <fanyijie@frogsing.com>
2021-11-12 09:43:13 +0800

    jj-button add style

--- src/components/jj/toolbar/listbtn.vue
+++ src/components/jj/toolbar/listbtn.vue
@@ -1,71 +1,40 @@
 <template>
-  <div class="jj_listbtn" v-if="data.column.data  instanceof Array">
-    <el-button-group v-if="data.editIndex==data.index">
-      <jj-button
-                 v-for="(btn) in data.column.data "
-                 v-if="btn.state&&btn.state=='edit'"
-:key="btn.event"
-:btn="btn"
-                 :row="data.row"
-                 @click="trigger(btn)"/>
-    </el-button-group>
-    <div v-else-if="data.editIndex<0">
-      <el-button-group>
-        <jj-button
-v-for="(btn) in data.column.data "
-v-if="!btn.state||btn.state=='normal'"
-:key="btn.event"
-:btn="btn"
-                   :row="data.row"
-                   @click="trigger(btn)"/>
-      </el-button-group>
 
-    </div>
-  </div>
-  <div class="jj_listbtn" v-else>
+  <div class="jj_listbtn">
     <el-button-group v-if="data.editIndex==data.index">
+      <template   v-for="(btn,index) in data.column.data.buttons|| data.column.data">
       <jj-button
-v-for="(btn,index) in data.column.data.buttons "
-v-if="btn.state&&btn.state=='edit'"
-:key="btn.event+index"
+                v-if="btn.state&&btn.state=='edit'"
+                :key="btn.event+index"
                  :btn="btn"
-:row="data.row"
+                  :row="data.row"
                  @click="trigger(btn)"/>
+      </template>
     </el-button-group>
-    <div v-else-if="data.editIndex<0">
+    <div v-if="data.editIndex<0">
       <el-button-group>
+        <template   v-for="(btn,index) in data.column.data.buttons|| data.column.data">
         <jj-button
-v-for="(btn,index) in data.column.data.buttons "
-v-if="!btn.state||btn.state=='normal'"
-:key="btn.event+index"
-                   :btn="btn"
-:row="data.row"
+                v-if="!btn.state||btn.state=='normal'"
+                :key="btn.event+index"
+                 :btn="btn"
+                :row="data.row"
                    @click="trigger(btn)"/>
+        </template>
       </el-button-group>
       <el-dropdown style="padding-left: 20px">
         <span class="el-dropdown-link">更多操作<i class="el-icon-arrow-down el-icon--right"></i></span>
-        <el-dropdown-menu v-if="data.editIndex==data.index">
-          <el-dropdown-item v-for="(btn,index) in data.column.data.list" :key="btn.event">
-            <jj-button
-v-if="btn.state&&btn.state=='edit'"
-:key="btn.event+index"
-:btn="btn"
-type="text"
-:row="data.row"
-                       @click="trigger(btn)"/>
-          </el-dropdown-item>
-        </el-dropdown-menu>
-        <el-dropdown-menu v-else-if="data.editIndex<0">
-          <el-dropdown-item v-for="(btn) in data.column.data.list " :key="btn.event">
-            <jj-button
-v-if="!btn.state||btn.state=='normal'"
-:key="btn.event"
-:btn="btn"
-type="text"
-:row="data.row"
-                       @click="trigger(btn)"/>
-          </el-dropdown-item>
-        </el-dropdown-menu>
+          <el-dropdown-menu>
+            <el-dropdown-item v-for="(btn) in data.column.data.list||[] " :key="btn.event">
+                  <jj-button
+                      v-if="!btn.state||btn.state=='normal'"
+                      :key="btn.event"
+                      :btn="btn"
+                      type="text"
+                      :row="data.row"
+                         @click="trigger(btn)"/>
+            </el-dropdown-item>
+          </el-dropdown-menu>
       </el-dropdown>
 
     </div>
