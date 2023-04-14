78cfe4e1eeffa8df04b20fb4f151ff182ac68f87
范益杰 <fanyijie@frogsing.com>
2023-04-14 23:51:46 +0800

    templet 支持 domain

--- src/components/jj/forms/xx-form-item.vue
+++ src/components/jj/forms/xx-form-item.vue
@@ -29,20 +29,25 @@
 
         if (_rules) {
           return _rules.map(x => {
-            return { _field: this.data.column.field, row: this.data.row, trigger: x.trigger, message: x.message, _pattern: x.pattern, validator: this.validator, _validator: this.getValidator(x), tooltips: this.tooltips }
+            return { ...x, _field: this.data.column.field, row: this.data.row, _pattern: x.pattern, validator: this.validator, _validator: this.getValidator(x), tooltips: this.tooltips }
           })
         } else { return [] }
       }
     },
     methods: {
       getValidator(x) {
-        if (x.validator) { return x }
+        if (x.validator) { return x.validator }
         if (x.required) {
           return this.requiredValidate
         }
         if (x.pattern) {
           return this.patternValidate
         }
+        return this.notExistValidate
+      },
+      notExistValidate(r, v, b) {
+        console.log('字段验证器定制错误', r)
+        b('字段验证器定制错误,请查看控制台')
       },
       requiredValidate(r, v, b) {
         if (v == null || typeof v == 'undefined' || v === '') {
@@ -57,6 +62,9 @@
       validator(r, v, callback) {
         v = this.getFieldValue(r.row, r._field)
         r._validator(r, v, function(obj) {
+          if (obj instanceof Error) {
+            obj = obj.message
+          }
           if (obj) {
             r.tooltips.tips = r.message ? r.message : obj
             r.tooltips.show = !!r.tooltips.tips
